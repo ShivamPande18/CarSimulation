6b2f8f853fb9d35fe0836d5a80f7e8231bc5f2fa
sashreekkrispy <sashreek.uyyala@gmail.com>
2023-07-19 22:09:45 +0530

    Update MouseRotation.cs

--- Assets/Scripts/MouseRotation.cs
+++ Assets/Scripts/MouseRotation.cs
@@ -12,8 +12,8 @@ public class MouseRotation : MonoBehaviour
 
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
 
     }
 
